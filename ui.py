e2a29aa65a2700ac2d898558f1496e3632d6c732
Dennis Kritchko <dennis_kritchko@sfu.ca>
2025-02-10 10:49:36 -0800

    fixed orientation issues for taller images

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, Label, Button
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ExifTags
 import cv2
 import os
 import subprocess
@@ -12,7 +12,7 @@ def select_image():
     file_path = filedialog.askopenfilename(filetypes= [("Image files", "*.jpg *.jpeg *.png")])
     if file_path:
         input_label.config(text=f"Selected: {os.path.basename(file_path)}")
-        display_image(file_path, original_image_label)
+        display_image(file_path, input_label)
         process_and_display_image(file_path)
 
 """"processes the image from fitpicify.py and displays the output image"""
@@ -33,6 +33,21 @@ def process_and_display_image(image_path):
 """displays the image on the label"""
 def display_image(image_path, label):
     image = Image.open(image_path)
+
+    try:
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                break
+        exif = dict(image._getexif().items())
+        if exif[orientation] == 3:
+            image = image.rotate(180, expand=True)
+        elif exif[orientation] == 6:
+            image = image.rotate(270, expand=True)
+        elif exif[orientation] == 8:
+            image = image.rotate(90, expand=True)
+    except (AttributeError, KeyError, IndexError):
+        pass
+
     image.thumbnail((300, 300))
     img = ImageTk.PhotoImage(image)
     label.config(image=img)
@@ -42,19 +57,17 @@ def display_image(image_path, label):
 root = tk.Tk()
 root.title("Fitpicify")
 root.geometry("640x480")
+root.configure(background="gray")
 
 
-input_label = Label(root, text="Select an image")
-input_label.pack()
-
-select_button = Button(root, text="Select Image", command=select_image)
+select_button = Button(root, text="Select Image to Edit", command=select_image, bg="light blue", fg="black")
 select_button.pack()
 
-original_image_label = Label(root)
-original_image_label.pack()
+input_label = Label(root)
+input_label.pack(side="left", padx=10, pady=10)
 
 edited_image_label = Label(root)
-edited_image_label.pack()
+edited_image_label.pack(side="right", padx=10, pady=10)
 
 """starts the main event loop"""
 root.mainloop()
